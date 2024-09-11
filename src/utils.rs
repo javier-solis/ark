8bb94b20eee4e6f4ed0c42c128bb8ed639031fcc
Javier Solis <javsolis@mit.edu>
2024-09-11 14:45:58 -0500

    add utils for printing

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,11 @@
+use colored::Colorize;
+
 use anyhow::{Context, Result};
+use colored_json::to_colored_json_auto;
 use jsonschema::JSONSchema;
 use std::{fs::File, io::Read};
+
+use crate::types::Course;
 
 pub fn read_file(file_path: &str) -> Result<String> {
   let mut file =
@@ -53,4 +58,63 @@ pub fn validate_json(file_path: &str, schema_path: &str) -> Result<serde_json::V
   }
 
   Ok(json_instance.clone()) // todo: does this have to be a .clone?
+}
+
+// todo: repurpose or remove
+fn print_result(year_0_entry: &serde_json::Value, json_instance: &serde_json::Value) {
+  println!("Year 0 entry (excluded from validation):");
+  println!("{}", to_colored_json_auto(year_0_entry).unwrap());
+
+  println!("JSON data (excluding year 0) is valid against the schema");
+  println!("Pretty-printed JSON:");
+  println!("{}", to_colored_json_auto(json_instance).unwrap());
+}
+
+/// just prints course stats. minimal keeps print small, to a reasonable minimum
+pub fn print_courses(courses: &[Course], minimal: bool) -> () {
+  // todo: I think minimal=false doesn't work
+
+  // todo: figure out sorting by ID, Units, title
+  if minimal {
+    println!("== Minimal Print ==");
+  } else {
+    println!("== Standard Print ==");
+  }
+
+  // todo: simplify implementation of formating (spacing)
+  println!("{:<8} {:<10} {}", "ID", "Units", "Title");
+
+  for course in courses {
+    // todo: print empty fields as "na", and warn user about that
+
+    let primary_id = &course.primary_id;
+    let transcript_title = &course.transcript_title;
+    let alternative_title = &course.alternative_title;
+    let units = course.units;
+
+    let formatted_line = if minimal {
+      let title = if !alternative_title.is_empty() && alternative_title != "na" {
+        alternative_title
+      } else {
+        transcript_title
+      };
+
+      format!("{:<8} {:<10} {}", primary_id, units, title)
+    } else {
+      let legacy_id = &course.legacy_id;
+      let dept = &course.dept;
+      let technical = course.technical;
+      let fulfills = &course.fulfills;
+
+      format!("") // todo
+    };
+
+    let has_todo = course.clone().into_iter().any(|field| field == "todo");
+
+    if has_todo {
+      println!("{}", formatted_line.red());
+    } else {
+      println!("{}", formatted_line);
+    }
+  }
 }
